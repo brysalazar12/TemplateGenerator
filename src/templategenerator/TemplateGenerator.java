8c86f2a0e940fa6147975d909e47ee332cd0b218
bryan <brysalazar12@gmail.com>
2015-07-10 17:33:40 +0800

    on-going

--- src/templategenerator/TemplateGenerator.java
+++ src/templategenerator/TemplateGenerator.java
@@ -10,10 +10,20 @@ package templategenerator;
  * @author Admin
  */
 import com.google.gson.Gson;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TemplateGenerator {
 	private String templateJson;
-	private String helpArgs = "-help";
+	private final String helpArgs = "-help";
+	private final String currentDir;
+	private final String fileType = "tpl";
+	protected Map params;
+	protected Map templateParams;
+	protected String FileName;
 
 	/**
 	 * @param args the command line arguments
@@ -21,21 +31,133 @@ public class TemplateGenerator {
 	public static void main(String[] args) {
 		// if no argument list all template available
 		// -t template -h list all arguments of this template and doc
+		TemplateGenerator generator = new TemplateGenerator();
+		generator.generate(args);
+	}
+
+	public TemplateGenerator() {
+		this.currentDir = System.getProperty("user.dir") + File.separatorChar;
 	}
 	
 	public void generate(String[] args) {
+		if(args.length < 1)
+		{
+			this.listAllTemplates();
+		} else {
+			this.readAllParameters(args);
+			try {
+				this.createTemplate(args);
+			} catch(Exception e) {
+				this.error(e.getMessage());
+			}
+		}
+	}
+
+	protected void createTemplate(String[] args) throws Exception {
+
+		// check if parameter -t exist
+		if(!this.params.containsKey("-t")) {
+			throw new Exception("Please specify the template. Ex. -t=templateName");
+		} else {
+			File template = new File(this.currentDir + this.params.get("-t") + this.fileType);
+
+			// check if template exist
+			if(!template.exists())
+				throw new Exception(this.params.get("-t") + " not exist. Please run the TemplateGenerator.jar without argument to list available template");
+
+			//read arguments of template
+			
+			if(args.length < 2) {
+				this.readTemplateParameter(template);
+			}
+		}
+	}
+
+	public void readTemplateParameter(File file) {
+		
+	}
+
+	protected void error(String error) {
+		System.err.println("\nError: " + error);
+	}
+
+	/**
+	 * Read parameter pass in cli
+	 * @param args 
+	 */
+	protected void readAllParameters(String[] args) {
+		this.params = new HashMap();
+		String[] param = null;
+		for (String arg : args) {
+			if(arg.contains("=")) {
+				param = arg.split("=");
+				this.params.put(param[0], param[1]);				
+			}
+		}
+	}
+
+	public void listAllTemplates() {
+		this.createHeader("List of Template");
+		File folder = new File(this.currentDir);
+		File[] files = folder.listFiles();
+		boolean hasTemplate = false;
+		BufferedReader reader = null;
+
+		// read all file name within the current directory
+		for (File file : files) {
+			if (file.isFile()) {
+				String name = file.getName();
+				try {
+					// read only the file with template extension
+					String type = name.substring(name.lastIndexOf(this.fileType));
+					int index = name.lastIndexOf(".");
+					if(index > 0) {
+						if(name.substring(index + 1).equals(this.fileType)) {
+							hasTemplate = true;
+							reader = new BufferedReader(new FileReader(file));
+							String doc = reader.readLine();
+							System.out.println(name.substring(0, index) + ": " + doc);
+						}
+					}
+				} catch (Exception e) {
+					// disregard filename without extension
+				}
+			}
+		}
+		if(!hasTemplate) {
+			System.err.println("No Available Template");
+		} else {
+			this.basicUsage();
+		}
+	}
+
+	protected void basicUsage() {
+		System.out.println("\n\nBasic Usage: java -jar TemplateGenerator.jar fileName -t=templateName\n");
+	}
+
+	protected void templateUsage() {
 
 	}
 
-	public void readTemplateJson() {
-
+	/**
+	 * Display the header inside the box
+	 * @param header 
+	 */
+	public void createHeader(String header) {
+		String box = "";
+		String line = "*";
+		int maxLength = header.length() + 8;
+		for(int i = 0; i < maxLength; i++) {
+			line+="-";
+		}
+		line+="*\n";
+		box+=line;
+		box+="|    " + header + "    |\n";
+		box+=line;
+		System.out.println(box);
 	}
 
-	public void readDoc() {
-
-	}
 }
-
 // TemplateGenerator.jar templateName param1 param2 param3
 
-//TemplateGenerator.jar -t MyModel -cls=User -id=user_id -pup=[username,password,age,location]
+//TemplateGenerator.jar fileName -t MyModel -cls=User -id=user_id -pup=[username,password,age,location]
