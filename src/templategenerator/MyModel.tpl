fa397dd9a759f3e8e95bee0c1174f50632eed144
bryan <brysalazar12@gmail.com>
2015-07-10 14:38:06 +0800

    build.xml

---
+++ src/templategenerator/MyModel.tpl
@@ -0,0 +1,27 @@
+@start
+
+cls:clsName,doc:Name of model
+id:id,doc:Primary key -id:user_id
+pup:publicProperties,doc:List of public properties. Ex: -pup=[firstName,lastName,age,location]
+prp:privateProperties,doc:List of private properties.
+
+@end
+
+<?php
+
+class {clsName} extends Model {
+
+	/**
+	* Primary key
+	*/
+	public ${id}
+
+	public ${publicProperties}
+
+	private ${privateProperties}
+
+	public function rules()
+	{
+		return [];
+	}
+}
--- src/templategenerator/TemplateGenerator.java
+++ src/templategenerator/TemplateGenerator.java
@@ -9,13 +9,33 @@ package templategenerator;
  *
  * @author Admin
  */
+import com.google.gson.Gson;
+
 public class TemplateGenerator {
+	private String templateJson;
+	private String helpArgs = "-help";
 
 	/**
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		// TODO code application logic here
+		// if no argument list all template available
+		// -t template -h list all arguments of this template and doc
 	}
 	
+	public void generate(String[] args) {
+
+	}
+
+	public void readTemplateJson() {
+
+	}
+
+	public void readDoc() {
+
+	}
 }
+
+// TemplateGenerator.jar templateName param1 param2 param3
+
+//TemplateGenerator.jar -t MyModel -cls=User -id=user_id -pup=[username,password,age,location]
